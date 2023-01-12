a5b0800258683b6db646bc73c2ceccde1290f884
Jhonatan Rampim <jhonatan.rampim@gmail.com>
2023-01-12 11:05:25 -0300

    base index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,10 +1,3 @@
 <?php
 
-use App\PaymentGateway\Paddle\Transaction;
-
-
 require_once __DIR__ . '/../vendor/autoload.php';
-
-$paddleTransaction = new Transaction();
-
-echo $paddleTransaction::STATUS_DECLINED;
